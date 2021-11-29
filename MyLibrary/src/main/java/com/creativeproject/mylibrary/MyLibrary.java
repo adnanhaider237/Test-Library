de7a7582ca0d7a1723c7f432767db0d841e57242
adnanhaider237 <adnanhaider237@gmail.com>
2021-11-29 18:55:23 +0500

    Initial commit

---
+++ MyLibrary/src/main/java/com/creativeproject/mylibrary/MyLibrary.java
@@ -0,0 +1,10 @@
+package com.creativeproject.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class MyLibrary {
+    public MyLibrary(Context context) {
+        Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+    }
+}
